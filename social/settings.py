b75fd13042e0538bcce74ebac90db7e5d90b3853
daniel love <danlove99@hotmail.co.uk>
2019-02-21 13:15:57 +0000

    Update settings.py

--- social/settings.py
+++ social/settings.py
@@ -136,5 +136,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'danloveprogramming@gmail.com'
-EMAIL_HOST_PASSWORD = 'daniellove99'
+EMAIL_HOST_USER = 'YOUR EMAIL'
+EMAIL_HOST_PASSWORD = 'YOUR PASSWORD'
